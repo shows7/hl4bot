9812fb851219310ba721c45cf2ef27113bafc7ce
shows7 <blazekom@gmail.com>
2015-10-03 13:47:22 -0400

    Update obot.py
    CENSORED INFO ON APP

--- obot.py
+++ obot.py
@@ -1,11 +1,11 @@
-app_id = "5LZup0e_hS2BkQ"
-app_secret = 'xExwe0N1HEEdHmZux-wE28kkvN0'
-app_uri = 'https://127.0.0.1:65010/authorize_callback'
-app_ua = 'HL4 thing'
+app_id = "XXXXXXXXXX"
+app_secret = 'XXXXXXXXXXX'
+app_uri = 'XXXXXXXXXX'
+app_ua = 'XXXXXXXXXXX'
 
 app_scopes = 'account creddits edit flair history identity livemanage modconfig modcontributors modflair modlog modothers modposts modself modwiki mysubreddits privatemessages read report save submit subscribe vote wikiedit wikiread'
-app_account_code = 'gMta8R-H_lPKCyJsOsCkLL1obNw'
-app_refresh = '39689937-c8XPK98B98xwRU7hT4ke57QP5Eo'
+app_account_code = 'XXXXXXXXX'
+app_refresh = 'XXXXXXXXXXXXXXXXXXXXXXXXXXX'
 
 import praw
 def login():
